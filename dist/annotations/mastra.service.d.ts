ed63db7016deed5b16103ca9420d5e5b905d2589
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2026-05-10 15:18:08 +0530

    chore: remove tracked dist folder

--- dist/annotations/mastra.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { AnnotationsService } from './annotations.service';
-export declare class MastraService {
-    private annotationsService;
-    private readonly logger;
-    constructor(annotationsService: AnnotationsService);
-    analyzeDocumentBackground(documentId: string, text: string): void;
-    private runAnalysis;
-    private fallbackMock;
-}
